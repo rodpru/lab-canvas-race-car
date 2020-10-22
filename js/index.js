bbe2c24aead8bba57ef5b2e564d7a03d0408e65f
rodpru <rgp.prt@gmail.com>
2020-10-22 19:14:53 +0100

    car ready

--- js/index.js
+++ js/index.js
@@ -1,7 +1,36 @@
+
+
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
+  
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
-  function startGame() {}
+const road = new Image();
+road.src = '/images/road.png';
+
+const car = new Image();
+car.src = '/images/car.png'
+
+
+function startGame() {
+  ctx.drawImage(road, 0, 0, 500, 700);
+  ctx.drawImage(car, 120, 370, 40, 70);
+}
 };
+ class car {
+   constructor() {
+     this.x = 120;
+     this.y = 230;
+   } 
+   moveLeft() {
+     this.x -= 25;
+   }
+   moveRight(){
+     this.x += 25;
+   }
+ }
+
